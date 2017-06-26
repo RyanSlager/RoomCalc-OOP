f7e306235c6ff5692e5b3d67655c89e8b4b54639
danteCLRK <fryanslaucerers@gmail.com>
2017-06-26 16:16:06 -0400

    added comments and a new method to the Room class

--- RoomCalc_OOP/RoomCalc_OOP/Program.cs
+++ RoomCalc_OOP/RoomCalc_OOP/Program.cs
@@ -10,7 +10,10 @@ namespace RoomCalc_OOP
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            // cont is initialized to y in order to make sure that the while loop runs
+            // doubles are declared for width and length
+            // an instance of Room class is declared
             string cont = "y";
             double width;
             double length;
@@ -18,28 +21,37 @@ namespace RoomCalc_OOP
 
             while (cont == "y")
             {
+                // ensures that the user enters a valid number for both width and length
+
                 Console.WriteLine("Please enter the width of the room");
-                while (!double.TryParse(Console.ReadLine(), out width))
+                while (!double.TryParse(Console.ReadLine(), out width) && width > 0)
                 {
                     Console.WriteLine("Please enter the width in numerical format");
                 }
 
                 Console.WriteLine("Please enter the length of the room");
-                while (!double.TryParse(Console.ReadLine(), out length))
+                while (!double.TryParse(Console.ReadLine(), out length) && length > 0)
                 {
                     Console.WriteLine("Please enter the length in numerical format");
                 }
 
+                // newRoom is initialized with the width and length
+
                 newRoom = new Room(width, length);
+                
+                // newRoom PrintInfo method is called to print the area and perimeter of the room
+                // info is printed to the console
+                
+                string info = newRoom.PrintInfo();
 
-                double roomArea = newRoom.Area();
-                double roomPer = newRoom.Perimeter();
+                Console.WriteLine(info);
 
-                Console.WriteLine("The area of the room is {0}", roomArea);
-                Console.WriteLine("The perimeter of the room is {0}", roomPer);
+                //user is prompted to continue or quit
 
                 Console.WriteLine("Continue? (y/n)");
                 cont = Console.ReadLine();
+
+                // user input for cont is checked
 
                 while (cont != "y" && cont != "n")
                 {
