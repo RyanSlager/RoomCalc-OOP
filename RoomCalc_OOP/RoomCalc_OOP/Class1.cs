590dbf8979f1e95a1a9e6649acece0a64ed67897
danteCLRK <fryanslaucerers@gmail.com>
2017-06-26 16:17:42 -0400

    added comments and PrintInfo method to the Room Class

--- RoomCalc_OOP/RoomCalc_OOP/Class1.cs
+++ RoomCalc_OOP/RoomCalc_OOP/Class1.cs
@@ -26,5 +26,14 @@ namespace RoomCalc_OOP
         {
             return (2 * width) + (2 * length);
         }
+
+        public string PrintInfo()
+        {
+            double a = this.Area();
+            double p = this.Perimeter();
+
+            string info = $"The area of the room is {a} and the perimeter of the room is {p}";
+            return info;
+        }
     }
 }
